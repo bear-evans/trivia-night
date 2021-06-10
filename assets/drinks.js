6a0797e7dfa7e7da44136c535dae5a3750efc22e
jcorozco81 <jcorozco@gmail.com>
2021-06-09 21:12:36 -0400

    divided code into functions, added random and search button and fields

--- assets/drinks.js
+++ assets/drinks.js
@@ -1,14 +1,93 @@
 // ingredient list: https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list
 // Search by ingredient: www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
 
-var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+// By ID www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
 
+// Search cocktail by name www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+
+// Events Button Random
+var randomButton=$("#random-button");  
+randomButton.on('click', function (event) {
+  event.preventDefault();
+  randomDrink();
+})
+
+
+
+
+// Event Button Search
+var searchButton=$("#search-button");  
+searchButton.on('click', function (event) {
+  event.preventDefault();
+  console.log("clicked")
+  searchField=$('#search-field').val();
+
+  if(searchField ===" " || searchField ==="" || searchField ===null){   //validate field
+
+  // if(typeof searchField !== 'string'){
+
+    console.log("Empty");
+}
+else{
+
+  console.log(searchField);
+  byDrink(searchField);
+
+}
+$('#search-field').val("");
+})
+
+
+
+
+
+// function byIngredient(){
+//   var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         renderDrink(data);
+//       });
+//     }
+
+
+function byDrink(drinkTxt){
+  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+drinkTxt;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        renderDrink(data);
+      });
+    }
+
+
+
+
+
+
+// Random Drink Function
+function randomDrink(){
+var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
       console.log(data);
+      renderDrink(data);
+    });
+  }
+
+
+// Render Drink Function
+function renderDrink(data){
 
       $('#img-thumb').attr("src", data.drinks[0].strDrinkThumb);
       console.log(data.drinks[0].strDrinkThumb); //Thumb
@@ -65,17 +144,10 @@ fetch(requestUrl)
         console.log(data.drinks[0].strMeasure14);
         console.log(data.drinks[0].strMeasure15);
 
-
-
-
-
         console.log(data.drinks[0].strInstructions); //Instructions
         $("#instructions").text(data.drinks[0].strInstructions);
 
 
+      
 
-
-
-
-
-  });
+      }
